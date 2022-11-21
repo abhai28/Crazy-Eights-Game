5f3e9c8635dba3ab7ae67869d77052394b5fd312
Abhai Roop Gill <36809390+abhai28@users.noreply.github.com>
2022-11-21 18:03:13 -0500

    Framework classes

---
+++ src/main/java/com/example/comp4004f22a3101077008/Card.java
@@ -0,0 +1,10 @@
+package com.example.comp4004f22a3101077008;
+
+public class Card {
+    private String suit;
+    private String rank;
+    public Card (String s, String r){
+        this.suit = s;
+        this.rank = r;
+    }
+}
---
+++ src/main/java/com/example/comp4004f22a3101077008/Player.java
@@ -0,0 +1,12 @@
+package com.example.comp4004f22a3101077008;
+
+import java.util.ArrayList;
+
+public class Player {
+    private int playerID;
+    private ArrayList<Card> cards;
+    public Player(int id){
+        this.playerID = id;
+        this.cards = new ArrayList<>();
+    }
+}
