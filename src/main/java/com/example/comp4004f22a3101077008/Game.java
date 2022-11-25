50c678c27b7e0786eb8c72598803985349ebdcb9
Abhai Roop Gill <36809390+abhai28@users.noreply.github.com>
2022-11-25 16:56:39 -0500

    Functionality for shuffleDeck and dealCards

--- src/main/java/com/example/comp4004f22a3101077008/Game.java
+++ src/main/java/com/example/comp4004f22a3101077008/Game.java
@@ -1,11 +1,16 @@
 package com.example.comp4004f22a3101077008;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 
 public class Game {
     ArrayList<Player> players = new ArrayList<>();
     ArrayList<Card> cards = new ArrayList<>();
-    
+    Card topCard = new Card();
+    public void startGame(){
+
+    }
     public void populateDeck() {
         String [] suit = {"S","C","D","H"};
         String [] rank = {"A","2","3","4","5","6","7","8","9","10","J","Q","K"};
@@ -18,6 +23,20 @@ public class Game {
     }
 
     public void shuffleDeck() {
-        
+        Collections.shuffle(cards);
+    }
+
+    public boolean dealCards() {
+        if(cards.size() == 0){
+            return false;
+        }
+        else{
+            for(Player p : players){
+                for(int i=0;i<5;i++){
+                    p.addCard(cards.remove(0));
+                }
+            }
+            return true;
+        }
     }
 }
