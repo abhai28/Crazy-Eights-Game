27625ac87c430c3677b05acd7a19cd1726e27a11
Abhai Roop Gill <36809390+abhai28@users.noreply.github.com>
2022-11-25 16:16:11 -0500

    Functionality for populateDeck

--- src/main/java/com/example/comp4004f22a3101077008/Game.java
+++ src/main/java/com/example/comp4004f22a3101077008/Game.java
@@ -6,4 +6,14 @@ public class Game {
     ArrayList<Player> players = new ArrayList<>();
     ArrayList<Card> cards = new ArrayList<>();
 
+    public void populateDeck() {
+        String [] suit = {"S","C","D","H"};
+        String [] rank = {"A","2","3","4","5","6","7","8","9","10","J","Q","K"};
+        for (String s : suit) {
+            for (String value : rank) {
+                Card c = new Card(s, value);
+                cards.add(c);
+            }
+        }
+    }
 }
