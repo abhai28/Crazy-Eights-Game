bf78c144927493802856a61b6b5a1ec4dcaed55c
Abhai Roop Gill <36809390+abhai28@users.noreply.github.com>
2022-11-25 16:03:25 -0500

    Template of game class

---
+++ src/main/java/com/example/comp4004f22a3101077008/Game.java
@@ -0,0 +1,10 @@
+package com.example.comp4004f22a3101077008;
+
+import java.util.ArrayList;
+
+public class Game {
+    ArrayList<Player> players = new ArrayList<>();
+    ArrayList<Card> cards = new ArrayList<>();
+
+    
+}
